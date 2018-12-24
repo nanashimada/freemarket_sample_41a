c29982021f072e9a403221d5e25e952570104a48
nanashimada <shimadanana@shimadanananoMacBook-puro.local>
2018-12-24 17:40:43 +0900

    Connect to S3
    S3の設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, "git@github.com:nanashimada/freemarket_sample_41a.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.3.1'
+set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['/Users/shimadanana/.ssh/nanasn777.pem']
@@ -16,12 +16,15 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
+append :linked_files, 'config/master.key'
+
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
 end
+
 
 
 
